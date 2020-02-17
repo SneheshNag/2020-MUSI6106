d98d47f5e94b588843aff3c3b15eb2e38c6d90cc
Snehesh <nagsnehesh@gmail.com>
2020-02-17 00:34:22 -0500

    base code

---
+++ src/2020-MUSI6106/2020-MUSI6106/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2020-MUSI6106
+//
+//  Created by Snehesh Nag on 2/16/20.
+//  Copyright © 2020 Snehesh Nag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
